aa2f08a9000eac3036b12d7b64158dc7439949a0
Aqcee <nikitashtankowebdes@gmail.com>
2016-06-17 20:50:16 +0300

    first commit
    empty blank

---
+++ app/src/main/java/examplecom/geomarkers/MainLayoutActivity.java
@@ -0,0 +1,13 @@
+package examplecom.geomarkers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}

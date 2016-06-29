1f1d8a06a4bf267c89b1b83b22bed081028de01b
Virg29 <denjik505@mail.ru>
2016-06-29 20:33:56 +0300

    add OM add view elements

--- app/src/main/java/examplecom/geomarkers/MainLayoutActivity.java
+++ app/src/main/java/examplecom/geomarkers/MainLayoutActivity.java
@@ -1,17 +1,24 @@
 package examplecom.geomarkers;
 
+import android.app.ActionBar;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ActionMenuView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class MainLayoutActivity extends AppCompatActivity {
+public class MainLayoutActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +40,56 @@ public class MainLayoutActivity extends AppCompatActivity {
             lay.setTypeface(Typeface.createFromAsset(getAssets(), "Roboto.ttf"));
         }
     }
-    public void addmarker(View view){
-        Intent intObj = new Intent(this, SecondActivity.class);
-        startActivity(intObj);
+    public void addMarker(View view){
+        //Intent intObj = new Intent(this, SecondActivity.class);
+        //startActivity(intObj);
+        LinearLayout addbutton = (LinearLayout) findViewById(R.id.addbutton);
+        addbutton.removeAllViews();
+        LinearLayout layout = (LinearLayout)findViewById(R.id.others);
+        LinearLayout.LayoutParams viewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout addbutton2 = new LinearLayout(this);
+        addbutton2.setBottom(5);
+        addbutton2.setClickable(true);
+        addbutton2.setBackgroundResource(R.drawable.default_rectangle);
+        addbutton2.setOnClickListener(this);
+        addbutton2.setId(R.id.addbutton);
+        addbutton2.setHorizontalGravity(View.TEXT_ALIGNMENT_CENTER);
+        layout.addView(addbutton2);
+        ImageView imgview = new ImageView(this);
+        imgview.setBackgroundResource(R.mipmap.cross);
+        addbutton2.addView(imgview);
+
+
+
+    }
+    protected void redrawButtonAdd(View view){
+        LinearLayout addbutton = (LinearLayout) findViewById(R.id.addbutton);
+        addbutton.removeAllViews();
+        LinearLayout layout = (LinearLayout)findViewById(R.id.others);
+        LinearLayout.LayoutParams viewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout addbutton2 = new LinearLayout(this);
+        addbutton2.setBottom(5);
+        addbutton2.setClickable(true);
+        addbutton2.setBackgroundResource(R.drawable.default_rectangle);
+        addbutton2.setOnClickListener(this);
+        addbutton2.setId(R.id.addbutton);
+        addbutton2.setHorizontalGravity(View.TEXT_ALIGNMENT_CENTER);
+        layout.addView(addbutton2);
+        ImageView imgview = new ImageView(this);
+        imgview.setBackgroundResource(R.mipmap.cross);
+        addbutton2.addView(imgview);
     }
 
+    @Override
+    public void onClick(View v) {
+        LinearLayout layout = (LinearLayout)findViewById(R.id.others);
+        LinearLayout.LayoutParams viewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout newlayout = new LinearLayout(this);
+        newlayout.setBackgroundResource(R.drawable.default_rectangle);
+        newlayout.setOrientation(LinearLayout.HORIZONTAL);
+        newlayout.setBottom(5);
+        layout.addView(newlayout);
+        redrawButtonAdd(v);
 
+    }
 }

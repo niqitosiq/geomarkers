b7d27bda0d33f288c3f41a4259317cf37c921dc9
Virg29 <denjik505@mail.ru>
2016-06-30 15:18:59 +0300

    commit

---
+++ app/src/main/java/examplecom/geomarkers/SecondActivity.java
@@ -0,0 +1,16 @@
+package examplecom.geomarkers;
+
+
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}

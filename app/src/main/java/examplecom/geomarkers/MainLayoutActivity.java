ba122350ced9f3a9f27e892cd942da3e55ade7dd
Virg29 <denjik505@mail.ru>
2016-07-02 18:50:21 +0300

    реализован полный функционал первого активити,исправлены баги

--- app/src/main/java/examplecom/geomarkers/MainLayoutActivity.java
+++ app/src/main/java/examplecom/geomarkers/MainLayoutActivity.java
@@ -19,6 +19,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ActionMenuView;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -58,12 +59,20 @@ public class MainLayoutActivity extends AppCompatActivity implements View.OnClic
         //       скр
         Boolean swpos = getIntent().getBooleanExtra("boolswitch",false);//получаю данные с СекондАктивити.класс
         ContentValues cv = new ContentValues();//временная таблица данных ,которые получаем из СекондАктивити.класс
-        cv.put("name",nametext);
-        cv.put("description", description);
-        int signal=0;
-        if(swpos){signal=1;}//переврдим булеан тип в интеджер для хранения в БД
-        cv.put("signal",signal);
-        db.insert("geomarkers",null,cv);
+        if(nametext!=null) {
+            cv.put("name", nametext);
+            cv.put("description", description);
+            int signal = 0;
+            if (swpos) {
+                signal = 1;
+            }//переврдим булеан тип в интеджер для хранения в БД
+            cv.put("signal", signal);
+            db.insert("geomarkers", null, cv);
+        }
+
+
+
+
         //запросы к БД + построение списка
         Cursor cursor = db.query("geomarkers",null,null,null,null,null,null);
         if(cursor.moveToFirst()){
@@ -83,36 +92,6 @@ public class MainLayoutActivity extends AppCompatActivity implements View.OnClic
                 tv.setTextSize(15);//пишем текст на метке
                 tv.setGravity(Gravity.CENTER);
                 marketbutton.addView(tv);
-
-
-
-
-
-
-              /*  int id = cursor.getInt(columnIdIndex);
-                String name = cursor.getString(columnNameIndex);
-                LinearLayout layout = (LinearLayout) findViewById(R.id.others);
-                LinearLayout addedbutton = new LinearLayout(this);
-                addedbutton.setBottom(5);                                                           //создание меток в списке
-                addedbutton.setClickable(false);//позже расширить функциОНАЛ
-                addedbutton.setBackgroundResource(R.drawable.default_rectangle);
-                addedbutton.setOnClickListener(this);                                                   //мертвый кусок кода потом убрать если не нужен будет
-                addedbutton.setHorizontalGravity(View.TEXT_ALIGNMENT_CENTER);
-                // Добавление
-                try {
-                    layout.addView(addedbutton, id);
-                }catch (Exception e){}
-                TextView tv = new TextView(this);                                                   //пишем текст на метке
-                tv.setText(name);
-                tv.setTextSize(15);
-                addedbutton.addView(tv);//Никита, кастомизируй текст ну там белый цвет,посерединки    */
-
-
-
-
-
-
-
             }while(cursor.moveToNext());
         }
         else{
@@ -121,45 +100,30 @@ public class MainLayoutActivity extends AppCompatActivity implements View.OnClic
         cursor.close();
         dbHelper.close();
 
-       //LinearLayout layout = (LinearLayout) findViewById(R.id.others);
+
         LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         LinearLayout addbutton = new LinearLayout(this);
         int gravity = Gravity.CENTER;
         lParams.gravity = gravity;
+        addbutton.setOrientation(LinearLayout.VERTICAL);
         addbutton.setBottom(5);
         addbutton.setBackgroundResource(R.drawable.default_rectangle);
         addbutton.setOnClickListener(this);
-        layout.addView(addbutton,lParams);
+        addbutton.setId(R.id.addbutton);
+        layout.addView(addbutton, lParams);
+        LinearLayout addplus = (LinearLayout) findViewById(R.id.addbutton);
         ImageView imgv = new ImageView(this);
         imgv.setBackgroundResource(R.drawable.plus);
-        layout.addView(imgv);
-    }
+        LinearLayout.LayoutParams plusParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        plusParams.gravity = gravity;
+        plusParams.setMargins(10,10,10,10);
+        plusParams.gravity = Gravity.CENTER;
 
-
-     public void addMarker(View v) {
-        Intent intObj = new Intent(this, SecondActivity.class);
-        startActivity(intObj);
-       // String params = "хер";
-       // addWithParams(params);
-    }
-
-   @SuppressLint("LongLogTag") // для вывода в лог большего объема текста.
-    private void addWithParams(String params) {
-        LinearLayout layout = (LinearLayout) findViewById(R.id.others);
-        int inputNumber = (layout.getChildCount() - 1);
-        //вывод кол-ва дочерних элементов в логи
-        Log.i("количество дочерних элементов", String.valueOf(inputNumber));
-        // Инициализация вида и параметров будущей кнопки                                           //позже убрать (в случае ненадобности)
-        LinearLayout addedbutton = new LinearLayout(this);
-        addedbutton.setBottom(5);
-        addedbutton.setClickable(true);
-        addedbutton.setBackgroundResource(R.drawable.default_rectangle);
-        addedbutton.setOnClickListener(this);
-        addedbutton.setHorizontalGravity(View.TEXT_ALIGNMENT_CENTER);
-        // Добавление
-        layout.addView(addedbutton, inputNumber);
+        addplus.addView(imgv, plusParams);
 
     }
+
+
 
 
     @Override

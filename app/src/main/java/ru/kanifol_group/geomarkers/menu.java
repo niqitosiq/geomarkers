d457c7437d7f15c94f2759120a7f5882c55ba270
Aqcee <nikitashtankowebdes@gmail.com>
2017-03-12 16:17:16 +0300

    Два рабочих экрана >доделать шрифты

--- app/src/main/java/ru/kanifol_group/geomarkers/menu.java
+++ app/src/main/java/ru/kanifol_group/geomarkers/menu.java
@@ -1,13 +1,19 @@
 package ru.kanifol_group.geomarkers;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class menu extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Integer[] MassIntro = new Integer[]{R.id.TodoList};
+        for (Integer i = 0; i < MassIntro.length; i++) {
+            TextView lay = (TextView) findViewById(MassIntro[i]);
+            lay.setTypeface(Typeface.createFromAsset(getAssets(), "Intro.otf"));
+        }
     }
 }

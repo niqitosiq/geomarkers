73c023d042c7594457f64618e3ae2f98ac1fd50c
Aqcee <nikitashtankowebdes@gmail.com>
2017-03-26 12:36:37 +0300

    Add notes commit

--- app/src/main/java/ru/kanifol_group/geomarkers/menu.java
+++ app/src/main/java/ru/kanifol_group/geomarkers/menu.java
@@ -1,18 +1,23 @@
 package ru.kanifol_group.geomarkers;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class menu extends AppCompatActivity {
+    int id = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +27,41 @@ public class menu extends AppCompatActivity {
             TextView lay = (TextView) findViewById(MassIntro[i]);
             lay.setTypeface(Typeface.createFromAsset(getAssets(), "Intro.otf"));
         }
-
+        LinearLayout layout = (LinearLayout) findViewById(R.id.maincontainer);
+        layout.removeAllViewsInLayout();
         makestartheight();// make start min height for note-block
+        DBHelper dbHelper = new DBHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor cursor = db.query("geomarkers",null,null,null,null,null,null);
+        if(cursor.moveToFirst()){
+            int columnIdIndex = cursor.getColumnIndex("id");
+            int columnNameIndex = cursor.getColumnIndex("name");
+            int columnDescIndex = cursor.getColumnIndex("description");
+            int j =0;
+            do{
+                int id = cursor.getInt(columnIdIndex);
+                String name = cursor.getString(columnNameIndex);
+                String description = cursor.getString(columnDescIndex);
+                LayoutInflater inflater = LayoutInflater.from(this);
+                LinearLayout custom_layout = (LinearLayout) inflater.inflate(R.layout.layout_default, null, false);
+                TextView textmarker = (TextView) custom_layout.findViewById(R.id.name);
+                TextView desc = (TextView) custom_layout.findViewById(R.id.descriptionview);
+                desc.setText(description);
+                custom_layout.setId(id);
+                textmarker.setText(name);
+                layout.addView(custom_layout);
+                Integer[] MassRoboto = new Integer[]{R.id.name, R.id.descriptionview};
+                for (Integer i = 0; i < MassRoboto.length; i++) {
+                    TextView lay = (TextView) custom_layout.findViewById(MassRoboto[i]);
+                    lay.setTypeface(Typeface.createFromAsset(getAssets(), "Roboto.ttf"));
+                }
+                j++;
+            }while(cursor.moveToNext());
+        }
+        else{
+        }
+        cursor.close();
+        dbHelper.close();
 
     }
 
--- app/src/main/java/ru/kanifol_group/geomarkers/settings.java
+++ app/src/main/java/ru/kanifol_group/geomarkers/settings.java
@@ -1,20 +1,41 @@
 package ru.kanifol_group.geomarkers;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.id;
 
 /**
  * Created by nikita on 12.03.17.
  */
 
 public class settings extends AppCompatActivity {
+
+    int id = 0;
+    double longitude;
+    double latitude;
+    double longitudeFromMap;
+    double latitudeFromMap;
+    int radius;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        EditText nameField = (EditText)findViewById(R.id.title_edit);
+        EditText descField = (EditText)findViewById(R.id.description_edit);
+        //SettingUp custom fonts
         Integer[] MassIntro = new Integer[]{R.id.mainText};
         for (Integer i = 0; i < MassIntro.length; i++) {
             TextView lay = (TextView) findViewById(MassIntro[i]);
@@ -25,5 +46,91 @@ public class settings extends AppCompatActivity {
             TextView lay = (TextView) findViewById(MassRoboto[i]);
             lay.setTypeface(Typeface.createFromAsset(getAssets(), "Roboto.ttf"));
         }
+
+        DBHelper dbHelper = new DBHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        Cursor cursor = db.query("geomarkers", null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            int columnIdIndex = cursor.getColumnIndex("id");
+            int columnNameIndex = cursor.getColumnIndex("name");
+            int columnDescriptionIndex = cursor.getColumnIndex("description");
+            int columnLatitudeIndex = cursor.getColumnIndex("latitude");
+            int columnLongitudeIndex = cursor.getColumnIndex("longitude");
+            int columnSignalIndex = cursor.getColumnIndex("signal");
+            int idintent = getIntent().getIntExtra("id", 0);
+            do {
+                id = cursor.getInt(columnIdIndex);
+                if (id==idintent){
+                    String name = cursor.getString(columnNameIndex);
+                    String description = cursor.getString(columnDescriptionIndex);
+                    int signalint = cursor.getInt(columnSignalIndex);
+                    latitude = cursor.getDouble(columnLatitudeIndex);
+                    longitude = cursor.getDouble(columnLongitudeIndex);
+                    boolean signal = false;
+                    if(signalint==1){signal = true;}
+                    TextView namev = (TextView)findViewById(R.id.title_edit);
+                    TextView descv = (TextView)findViewById(R.id.description_edit);
+                    namev.setText(name);
+                    descv.setText(description);
+                    //deletebutton.setClickable(true);
+
+                }
+                else{
+                    db.close();
+                }
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
+        db.close();
+
+    }
+    public void okButton(View v){
+        int idintent = getIntent().getIntExtra("id",0);// Получение айди элемента на который было совершено покушение
+        if (idintent == 0) {
+
+            ContentValues cv = new ContentValues();
+            EditText nametext = (EditText) findViewById(R.id.title_edit);
+            EditText descriptiontext = (EditText) findViewById(R.id.description_edit);//обьявляеем поля ввода и свитч
+            if (nametext.getText().toString().length() < 1) {
+                Toast toast = Toast.makeText(getApplicationContext(), //проверяем полность 1-ого поля ввода
+                        "Заполните первое поле", Toast.LENGTH_SHORT);
+                toast.show();
+            } else {
+
+                cv.put("name", nametext.getText().toString());
+                //подготавливаем данные к отправке
+                cv.put("description", descriptiontext.getText().toString());
+                cv.put("latitude", latitudeFromMap);
+                cv.put("longitude", longitudeFromMap);
+                cv.put("radius", radius);
+                DBHelper dbHelper = new DBHelper(this);
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.insert("geomarkers", null, cv);
+                db.close();
+                Log.d("db", "insert");
+                Intent intent = new Intent(this, menu.class);
+                startActivity(intent);
+            }
+        } else {
+            EditText nametext = (EditText) findViewById(R.id.title_edit);
+            EditText descriptiontext = (EditText) findViewById(R.id.description_edit);//обьявляеем поля ввода и свитч
+            ContentValues cv = new ContentValues();
+            cv.put("name", nametext.getText().toString());
+            cv.put("description", descriptiontext.getText().toString());
+            cv.put("latitude",latitudeFromMap);
+            cv.put("longitude",longitudeFromMap);
+            DBHelper dbHelper = new DBHelper(this);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            db.update("geomarkers", cv, "id = ?", new String[]{Integer.toString(idintent)});
+            db.close();
+            Log.d("db", "update"+idintent);
+            Intent intent = new Intent(this, menu.class);
+            startActivity(intent);
+
+
+        }
+
+
     }
 }
